8ee16bc3a6699ae54a61177ced1881f99bf4552d
1MuRaD12 <tu71tfsws@code.edu.az>
2022-03-15 10:49:30 +0300

    string

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "bU tasKI metHodla yAzmAliSiz";
+
+            Console.WriteLine(big(name));
+
+            //name = name.ToLower();
+
+            //Console.WriteLine(name);
+            
+        }
+        public static string big (string name)
+        {
+            name = name.Replace("bU", "Bu");
+            name = name.Replace("tasKI", "Taski");
+            name = name.Replace("metHodla", "Methodla");
+            name = name.Replace("yAzmAliSiz", "Yazmalisiz");
+            
+            {
+                return name;
+            }
+        }
+    }
+}
